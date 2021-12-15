f8303a38d9091a5646b35cbb296d9bfd1aa7a9aa
kodefill <samarinda.dev@gmail.com>
2021-12-15 10:12:31 +0800

    git sekuensial

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+semua sintaksis dasar bahasa pemrograman dari :
+1. Sekuensial :  langkah Berurutan
+2. Percabangan : langkah Melompat jika Kondisi terpenuhi
+3. Perulangan : Mengulang langkash yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# Sekuensial
+print('ibu berkata, "pergi ke toko"')
+print('budi menjawab,"baik apa yang harus saya lakukan di toko?"')
+print('ibu menjawab,"belikan satu botol susu, dan jika ada telor beli 6"')
+print("maka budi berangkat ke toko")
+print("dan mulai berbelanja")
