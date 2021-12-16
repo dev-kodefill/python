529204bf7a518852418e2cef3b2df09e82c6d7e9
kodefill <samarinda.dev@gmail.com>
2021-12-16 22:34:25 +0800

    git sekuensial

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ print('budi menjawab,"baik apa yang harus saya lakukan di toko?"')
 print('ibu menjawab,"belikan satu botol susu, dan jika ada telor beli 6"')
 print("maka budi berangkat ke toko")
 print("dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu > 0:
+    print("budi mengecek harganya, dan ternyata uangnya cukup")
+    print("budi membeli 1 botol susu")
+else:
+    print("budi tidak jadi membeli 1 botol susu")
+    print("memerikan susu kepada ibu")
